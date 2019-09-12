3053ad24ce2e5d129129e17ae636dd91c757a830
ian <29166333+sailingbikeruk@users.noreply.github.com>
2019-09-12 15:42:10 +0100

    Initial Commit

---
+++ Get-AdminAccounts.ps1
@@ -0,0 +1,72 @@
+Import-Module Activedirectory
+
+# Lines below are interchanged for operation or testing - Get-ADForest is used in normal operation, the manual config is used in testing
+$Domains = (Get-ADforest).domains
+#$Domains = "grafman.graftonplc.net","graftonho.graftonplc.net
+
+$ForestPrivGroups = "Enterprise Admins", "Schema Admins"
+$DomainPrivGroups = "Administrators", "Account Operators", "Server Operators", "Backup Operators", "Domain Admins"
+$Members = @()
+$MemberDetails = @()
+$colMembers = @()
+$Users = @()
+
+foreach ($group in $ForestPrivGroups) {
+    $Members = @() 
+    $Domain = "graftonplc.net"     
+    Write-Host "Enumerating $Group" -foreground Blue        
+    $Members = Get-ADGroupMember $Group -server $Domain -Recursive         
+    $Count = $Members.count                
+    Write-Host "Found $count members in $Group"        
+    foreach ($member in $Members) {            
+        $MembersDetails = get-aduser $member -properties * | Select-Object DisplayName, Enabled, LastLogonDate, PasswordLastSet, PasswordNeverExpires, SamAccountName, UserPrincipalName, Description, @{Name = 'PasswordAge'; Expression = { ((Get-Date) - $_.PasswordLastSet).Days } }                      
+        $FullDetails = [PSCustomObject]@{                
+            DisplayName          = $MembersDetails.DisplayName 
+            Enabled              = $MembersDetails.Enabled               
+            LastLogonDate        = $MembersDetails.LastLogonDate               
+            PasswordAge          = $MembersDetails.PasswordAge                
+            PasswordLastSet      = $MembersDetails.PasswordLastSet                
+            PasswordNeverExpires = $MembersDetails.PasswordNeverExpires                
+            SamAccountName       = $MembersDetails.SamAccountName                
+            UserPrincipalName    = $MembersDetails.UserPrincipalName                
+            Description          = $MembersDetails.Description                
+            Domain               = $Domain                
+            Group                = $Group            
+        }            
+        $colMembers += $FullDetails 
+    }
+}
+foreach ($Domain in $Domains) {
+    Write-Host "Enumerating Groups in $Domain" -ForegroundColor Yellow
+    foreach ($Group in $DomainPrivGroups) {
+        $Members = @()
+        Write-Host "Enumerating $Group" -foreground Blue
+        $Members = Get-ADGroupMember $Group -server $Domain -Recursive 
+        $Count = $Members.count        
+        Write-Host "Found $count members in $Group"
+        foreach ($member in $Members) {
+            $MembersDetails = get-aduser $member -properties * | Select-Object DisplayName, Enabled, LastLogonDate, PasswordLastSet, PasswordNeverExpires, SamAccountName, UserPrincipalName, Description, @{Name = 'PasswordAge'; Expression = { ((Get-Date) - $_.PasswordLastSet).Days } }          
+            $FullDetails = [PSCustomObject]@{
+                DisplayName          = $MembersDetails.DisplayName
+                Enabled              = $MembersDetails.Enabled
+                LastLogonDate        = $MembersDetails.LastLogonDate
+                PasswordAge          = $MembersDetails.PasswordAge
+                PasswordLastSet      = $MembersDetails.PasswordLastSet
+                PasswordNeverExpires = $MembersDetails.PasswordNeverExpires
+                SamAccountName       = $MembersDetails.SamAccountName
+                UserPrincipalName    = $MembersDetails.UserPrincipalName
+                Description          = $MembersDetails.Description
+                Domain               = $Domain
+                Group                = $Group
+            }
+            $colMembers += $FullDetails 
+        }
+
+    }
+}
+write-host "`n`nwriting CSV file to E:\Scripts\Ian\CSV\PrivUsers\AllPrivUsers.csv " -ForegroundColor Green
+$colMembers | export-csv -Path E:\Scripts\Ian\CSV\PrivUsers\AllPrivUsers.csv -NoTypeInformation
+
+
+
+
